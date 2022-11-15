48b4a03a25e5c6105a6e3b1a255eeae92a051277
jdonlucas <jdonlucas93@gmail.com>
2022-11-15 17:12:09 -0600

    init project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { MapContainer } from 'react-leaflet/MapContainer'
+import { TileLayer } from 'react-leaflet/TileLayer'
+
 import './App.css';
+import 'leaflet/dist/leaflet.css';
 
 function App() {
+
+  const [day,setDay] = useState(true);
+
+  const toggleTime = () => {
+    let body = document.getElementsByTagName('body');
+    let title = document.getElementById("app-title")
+
+    if(!day) {
+      // title
+      title.classList.add('light');
+      title.classList.remove('dark');
+
+      // body
+      body[0].classList.add('light');
+      body[0].classList.remove('dark');
+    } else {
+      // title
+      title.classList.add('dark');
+      title.classList.remove('light');
+
+      // body
+      body[0].classList.add('dark');
+      body[0].classList.remove('light');
+    }
+    setDay(!day)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1 id="app-title">Sonidos SNMB</h1>
+      </div>
+      <div className='toggle-switch-container'>
+        <div className="toggle-switch">
+            <label className='toggle-switch-label'>
+                <input 
+                  checked={day}
+                  onChange={() => toggleTime()}
+                  className='input-switch' 
+                  type="checkbox" />
+                <span className="slider"></span>
+            </label>
+        </div>
+      </div>
+      <MapContainer 
+        key={day}
+        className="map-snmb" 
+        center={[22.0458888889, -100.545444444]} 
+        zoom={5} 
+        scrollWheelZoom={false}>
+        { day ? <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+        /> : <TileLayer 
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+        />}
+      </MapContainer>
     </div>
   );
 }
