17c4672114e0c4baa6e1d2b213d7508c3ef5e142
jdonlucas <jdonlucas93@gmail.com>
2022-11-18 15:20:33 -0600

    adding play sound

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { MapContainer } from 'react-leaflet/MapContainer'
 import { TileLayer } from 'react-leaflet/TileLayer'
 import { GeoJSON } from 'react-leaflet';
@@ -11,6 +11,17 @@ function App() {
 
   const [day,setDay] = useState(true);
   const [anp,setANP] = useState(null);
+  const [isPlaying,setIsPlaying] = useState(false);
+  const [audio,setAudio] = useState(null);
+  const [nowPlaying,setPlay] = useState(null);
+
+  useEffect(() => {
+    if(audio && isPlaying) {
+      audio.play();
+    } else if (audio && !isPlaying) {
+      audio.pause();
+    }
+  })
 
   const toggleTime = () => {
     let body = document.getElementsByTagName('body');
@@ -55,6 +66,17 @@ function App() {
     setANP('')
   }
 
+  const playPauseSound = () => {
+    if(isPlaying) {
+      audio.pause();
+      setIsPlaying(false);
+    } else {
+      audio.play();
+      setIsPlaying(true);
+    }
+    
+  }
+
   const onClickAnp = async (e) => {
 
     let response = await fetch('https://sipecamdata.conabio.gob.mx/snmbgraphql/',{
@@ -80,8 +102,18 @@ function App() {
       })
     });
     let data = await response.json();
-
-    console.log(data.data.allAnpAudioSamples.edges[0].node.snmbArchivoPath)
+    
+    if(data.data.allAnpAudioSamples.edges.length) {
+      let audioMp3 = data.data.allAnpAudioSamples.edges[0].node.snmbArchivoPath
+        .replace('/LUSTRE/sacmod/snmb_data/','https://monitoreo.conabio.gob.mx/snmb_pics/')
+        .replace('wav','mp3')
+      setAudio(new Audio(audioMp3));
+      setIsPlaying(true);
+      setPlay(e.target.feature.properties.NOMBRE)
+    } else {
+      setIsPlaying(false);
+      setPlay(null)
+    }
   }
 
   const onEachFeature = (feature,layer) => {
@@ -115,8 +147,12 @@ function App() {
           {day ? 'Día' : ''}
         </div>
       </div>
-      <div className={`anp-name ${day ? 'light' : 'dark'}`}>
+      <div className={`anp-name  ${day ? 'light' : 'dark'}`}>
         {anp ? `ANP: ${anp}` : null}
+      </div>
+      <div className={`now-playing ${day ? 'light' : 'dark'}`}>
+        {nowPlaying ? `Reproduciendo sonidos de ANP ${nowPlaying}` : null}
+        {nowPlaying ? <div onClick={playPauseSound} className='play-pause'>{ isPlaying ? 'Pausar' : 'Reproducir'}</div> : null}
       </div>
       <MapContainer 
         key={day}
