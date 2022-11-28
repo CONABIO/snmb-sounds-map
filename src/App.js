baf79c7a07d3171cea266514f4f2470185ba7776
jdonlucas <jdonlucas93@gmail.com>
2022-11-28 13:10:59 -0600

    added more info and hide geoms

--- src/App.js
+++ src/App.js
@@ -9,11 +9,34 @@ import 'leaflet/dist/leaflet.css';
 
 function App() {
 
+  // sets if it should reproduce day or night sounds
   const [day,setDay] = useState(true);
+
+  // set anp display name on mouse hover
   const [anp,setANP] = useState(null);
+
+  // set boolean var to know if audio is paying
   const [isPlaying,setIsPlaying] = useState(false);
+
+  // sets the audio to play
   const [audio,setAudio] = useState(null);
+
+  // sets the current anp that is playing
   const [nowPlaying,setPlay] = useState(null);
+
+  // set current vegetation
+  const [vegetation,setVegetation] = useState(null);
+
+  // set sample hour
+  const [hour,setHour] = useState(null);
+
+  // sets the list of anp that have audio files
+  const [anpAudio,setAnpWithAudio] = useState([]);
+
+  useEffect(() => {
+    if(anpAudio.length !== 27)
+      getAnps();
+  })
 
   useEffect(() => {
     if(audio && isPlaying) {
@@ -22,6 +45,28 @@ function App() {
       audio.pause();
     }
   })
+
+  const getAnps = async () => {
+    let response = await fetch('https://sipecamdata.conabio.gob.mx/snmbgraphql/',{
+      method: 'post',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "query": `query {
+          anpWithAudio {
+            edges {
+              node
+            }
+          } 
+          
+        }`
+      })
+    })
+    let data = await response.json();
+    
+    setAnpWithAudio(data.data.anpWithAudio.edges.map(n => n.node))
+  }
 
   const toggleTime = () => {
     let body = document.getElementsByTagName('body');
@@ -56,18 +101,31 @@ function App() {
   }
 
   const style = (feature) => {
-    return {
-        fillColor: day ? "#02e3be" : "#ff8acc",
+    if(anpAudio.includes(feature.id))
+      return {
+          fillColor: day ? "#02e3be" : "#ff8acc",
+          weight: 0.3,
+          opacity: 1,
+          color: day ? "#02e3be" : "#ff8acc",
+          dashArray: '3',
+          fillOpacity: 0.5
+      };
+    else 
+      return {
+        fillColor: "#bababa",
         weight: 0.3,
-        opacity: 1,
-        color: day ? "#02e3be" : "#ff8acc",
+        opacity: 0.5,
+        color: "#bababa",
         dashArray: '3',
-        fillOpacity: 0.5
-    };
+        fillOpacity: 0.2
+      }
   };
 
   const showLabel = (e) => {
-    setANP(e.target.feature.properties.NOMBRE);
+    if(anpAudio.includes(e.target.feature))
+      setANP(e.target.feature.properties.NOMBRE);
+    else
+      setANP(e.target.feature.properties.NOMBRE + " (sin audios)");
   }
 
   const hideLabel = () => {
@@ -102,19 +160,19 @@ function App() {
             ${
               dayOrNight.checked ? 
               `sampleHour: {
-                greaterThan: 6,
-                lessThan: 18
+                greaterThan: 9,
+                lessThan: 20
               }` : 
               `
               or: [
                 {
                   sampleHour: {
-                    lessThan: 6
+                    lessThan: 9
                   }
                 },
                 {
                   sampleHour: {
-                    greaterThan: 18
+                    greaterThan: 20
                   }
                 }
               ]
@@ -124,6 +182,8 @@ function App() {
             edges {
               node {
                 snmbArchivoPath
+                snmbVegetacionTipo
+                sampleHour
               }
             }
           }
@@ -139,9 +199,13 @@ function App() {
       setAudio(new Audio(audioMp3));
       setIsPlaying(true);
       setPlay(anpName)
+      setVegetation(data.data.allAnpAudioSamples.edges[0].node.snmbVegetacionTipo);
+      setHour(data.data.allAnpAudioSamples.edges[0].node.sampleHour);
     } else {
       setIsPlaying(false);
-      setPlay(null)
+      setPlay(null);
+      setVegetation(null);
+      setHour(null);
     }
   }
 
@@ -163,7 +227,7 @@ function App() {
   return (
     <div className="App">
       <div>
-        <h1 id="app-title" className={day ?  'light' : 'dark'}>Sonidos SNMB</h1>
+        <h1 id="app-title" className={day ?  'light' : 'dark'}>Sonidos SNMB de áreas protegidas</h1>
       </div>
       <div className={`toggle-switch-container ${day ?  '' : 'dark'}`}>
         <div className={`daytime-name dark`}>
@@ -189,6 +253,8 @@ function App() {
       </div>
       <div className={`now-playing ${day ? 'light' : 'dark'}`}>
         {nowPlaying ? `Reproduciendo sonidos de ANP ${nowPlaying}` : null}
+        <div>{vegetation ? `Vegetación: ${vegetation}` : null}</div>
+        <div>{hour ? `Hora de grabación: ${hour}:00` : null}</div>
         {nowPlaying ? <div onClick={playPauseSound} className='play-pause'>{ isPlaying ? 'Pausar' : 'Reproducir'}</div> : null}
       </div>
       <MapContainer 
